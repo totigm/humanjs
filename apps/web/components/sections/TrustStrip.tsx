3dbc49b302ebe117343a217a9b0ac91961768f1c
Toti <totigm@gmail.com>
2026-06-16 01:44:12 -0300

    chore(web): bump landing version refs to playwright v0.9 / core 0.8.0

--- apps/web/components/sections/TrustStrip.tsx
+++ apps/web/components/sections/TrustStrip.tsx
@@ -12,7 +12,7 @@ interface Signal {
 const signals: Signal[] = [
   {
     label: 'Latest',
-    value: '@humanjs/core@0.7.0',
+    value: '@humanjs/core@0.8.0',
     href: 'https://www.npmjs.com/package/@humanjs/core',
     iconKind: 'external',
   },
